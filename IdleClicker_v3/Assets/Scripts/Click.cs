35c16afdb6ed0e5d85237d3c89e419ab58623f0c
Mark Hill <bullseyecoder15@gmail.com>
2016-01-19 22:55:12 -0500

    First commit v0.0.01

---
+++ IdleClicker_v3/Assets/Scripts/Click.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text fpc;
+    public UnityEngine.UI.Text fundsDisplay;
+    public float funds = 0;
+    public int fundsPerClick = 1;
+
+    void Update()
+    {
+        fundsDisplay.text = "Funds: $" + funds;
+        fpc.text = "Funds/Click: " + fundsPerClick;
+    }
+
+    public void Clicked()
+    {
+        funds += fundsPerClick;
+    }
+
+}
---
+++ IdleClicker_v3/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+    public int clickPower;
+    public string itemName;
+    public float baseCost;
+
+    void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: " + clickPower;
+    }
+
+    public void PurchasedUpgrade()
+    {
+        if(click.funds >= cost)
+        {
+            click.funds -= cost;
+            count += 1;
+            click.fundsPerClick += clickPower;
+            cost = baseCost * Convert.ToSingle(Math.Pow(1.15, count));
+            cost = Convert.ToSingle(Math.Round(cost));
+        }
+    }
+}
