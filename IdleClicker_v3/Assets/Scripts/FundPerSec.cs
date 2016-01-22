07590042f3553b9e81c516e812c08d27b2ef8327
Mark Hill <bullseyecoder15@gmail.com>
2016-01-21 20:53:10 -0500

    Done watching video 2

---
+++ IdleClicker_v3/Assets/Scripts/FundPerSec.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FundPerSec : MonoBehaviour {
+
+    public UnityEngine.UI.Text fpsDisplay;
+    public Click click;
+    public ItemManager[] items;
+
+    void Start()
+    {
+        StartCoroutine(AutoTick());
+    }
+
+    void Update()
+    {
+        fpsDisplay.text = GetFundPerSec() + " funds/sec.";
+    }
+
+    public int GetFundPerSec()
+    {
+        int tick = 0;
+        foreach(ItemManager item in items)
+        {
+            tick += item.count * item.tickValue;
+        }
+
+        return tick;
+    }
+
+    public void AutoFundPerSec()
+    {
+        click.funds += GetFundPerSec();
+    }
+
+    IEnumerator AutoTick()
+    {
+        while (true)
+        {
+            AutoFundPerSec();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+}
---
+++ IdleClicker_v3/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ItemManager : MonoBehaviour {
+
+    public UnityEngine.UI.Text itemInfo;
+    public Click click;
+    public float cost;
+    public int tickValue;
+    public int count;
+    public string itemName;
+    private float baseCost;
+
+    void Start()
+    {
+        baseCost = cost;
+    }
+
+    void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nGold: " + tickValue + "/s";
+    }
+
+    public void PurchasedItem()
+    {
+        if(click.funds >= cost)
+        {
+            click.funds -= cost;
+            count += 1;
+            cost = Mathf.Round(baseCost * Mathf.Pow(1.15f, count));
+            // cost = baseCost * Convert.ToSingle(Math.Pow(1.15, count));
+            // cost = Convert.ToSingle(Math.Round(cost));
+        }
+    }
+
+}
